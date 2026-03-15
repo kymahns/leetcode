bb035ff15777fa34bdb5b9692a48513ba8974830
Nam <89253211+kymahns@users.noreply.github.com>
2026-03-15 03:48:35 +0000

    Time: 19 ms (72.43%) | Memory: 85.6 MB (41.67%) - LeetSync

---
+++ DSA/84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        vector<int> PSE(n);
+        vector<int> NSE(n);
+        for(int i = 0; i<n;i++){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+            PSE[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        st = stack<int>();
+        for(int i = n-1; i>=0 ;i--){
+            while(!st.empty() && heights[st.top()] >= heights[i]){
+                st.pop();
+            }
+            NSE[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        int maxArea = 0;
+        for(int  i = 0; i<n; i++){
+            int area = heights[i] * (NSE[i] - PSE[i] - 1);
+            maxArea = max(maxArea,area);
+        }
+        return maxArea;
+    }
+};

328e36bfe18bafdc88126469c1e7645683b4fc97
Nam <89253211+kymahns@users.noreply.github.com>
2026-03-15 03:00:25 +0000

    Time: 28 ms (30.62%) | Memory: 107.5 MB (21.24%) - LeetSync

---
+++ DSA/739-daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> answer(temperatures.size(),0);
+        stack<int> st;
+        for(int i = 0; i< temperatures.size();i++){
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                answer[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return answer;
+    } 
+};

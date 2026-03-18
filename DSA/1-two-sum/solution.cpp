4aaacd4d4dc60da2d71fcf1716c4f93a1dc2513f
Nam <89253211+kymahns@users.noreply.github.com>
2026-03-18 18:37:37 +0000

    Time: 0 ms (100.00%) | Memory: 15 MB (18.22%) - LeetSmith

---
+++ DSA/1-two-sum/solution.cpp
@@ -0,0 +1,22 @@
+// Problem: Two Sum
+// URL: https://leetcode.com/problems/two-sum
+// Difficulty: Easy
+// Language: C++
+// Date: 2026-03-18
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        int i;
+        for(i = 0; i< nums.size() ; i++){
+            if(m.find(nums[i]) == m.end()){
+                m[target - nums[i]] = i;
+            }
+            else{
+                break;
+            }
+        }
+        return {m[nums[i]],i};
+    }
+};

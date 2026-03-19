3ed81424367adbdec75da712b1072ca1e641c0dc
Nam <89253211+kymahns@users.noreply.github.com>
2026-03-19 00:01:02 +0000

    Time: 0 ms (100.00%) | Memory: 63 MB (47.95%) - LeetSmith

---
+++ DSA/11-container-with-most-water/solution.cpp
@@ -0,0 +1,24 @@
+// Problem: Container With Most Water
+// URL: https://leetcode.com/problems/container-with-most-water
+// Difficulty: Medium
+// Language: C++
+// Date: 2026-03-19
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxArea = 0;
+        int i = 0, j = height.size()-1;
+        while(i<j){
+            int area = min(height[i],height[j]) * (j-i);
+            maxArea = max(maxArea,area);
+            if(height[i] > height[j]){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return maxArea;
+    }
+};

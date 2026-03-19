8ea27f6cd9ff84050cf546344f92641e4f172383
Nam <89253211+kymahns@users.noreply.github.com>
2026-03-19 00:02:13 +0000

    Time: 5 ms (67.67%) | Memory: 11.8 MB (64.68%) - LeetSmith

---
+++ DSA/3-longest-substring-without-repeating-characters/solution.cpp
@@ -0,0 +1,22 @@
+// Problem: Longest Substring Without Repeating Characters
+// URL: https://leetcode.com/problems/longest-substring-without-repeating-characters
+// Difficulty: Medium
+// Language: C++
+// Date: 2026-02-03
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> lastSeen;
+        int maxLen = 0, left = 0;
+        for (int right = 0; right < s.length(); ++right) {
+            char c = s[right];
+            if (lastSeen.count(c) && lastSeen[c] >= left) {
+                left = lastSeen[c] + 1;
+            }
+            lastSeen[c] = right;
+            maxLen = max(maxLen, right - left + 1);
+        }
+        return maxLen;
+    }
+};

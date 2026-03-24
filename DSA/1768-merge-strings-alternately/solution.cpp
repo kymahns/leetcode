9cd7c25ab7245cedddaf22782aae95ee798fedf9
Nam <89253211+kymahns@users.noreply.github.com>
2026-03-24 18:05:09 +0000

    Time: 0 ms (100.00%) | Memory: 8.5 MB (32.81%) - LeetSmith

--- DSA/1768-merge-strings-alternately/solution.cpp
+++ DSA/1768-merge-strings-alternately/solution.cpp
@@ -2,25 +2,23 @@
 // URL: https://leetcode.com/problems/merge-strings-alternately
 // Difficulty: Easy
 // Language: C++
-// Date: 2025-04-11
+// Date: 2026-03-24
 
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        int i = 0;
-        int n1 = word1.length(), n2 = word2.length();
-        string s = "";
-        while(i < n1 && i < n2){
-            s = s + word1[i] + word2[i++];
+        int i = 0, j = 0;
+        string word = "";
+        while(i < word1.length() || j < word2.length()){
+            if(i < word1.length()){
+                word+=word1[i];
+            }
+            if(j < word2.length()){
+                word+=word2[j];
+            }
+            i++;
+            j++;
         }
-        if(i == n1)
-            while(i < n2){
-                s+= word2[i++];
-            }
-        else
-            while(i < n1){
-                s+=word1[i++];
-            }
-        return s;
+        return word;
     }
 };

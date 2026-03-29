69c9caecd0fd64ed98d3f964c448d722c4c0755a
Nam <89253211+kymahns@users.noreply.github.com>
2026-03-29 00:24:01 +0000

    Time: 111 ms (33.75%) | Memory: 76.7 MB (17.43%) - LeetSmith

---
+++ DSA/1679-max-number-of-k-sum-pairs/solution.cpp
@@ -0,0 +1,23 @@
+// Problem: Max Number of K-Sum Pairs
+// URL: https://leetcode.com/problems/max-number-of-k-sum-pairs
+// Difficulty: Medium
+// Language: C++
+// Date: 2026-03-29
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> map;
+        int count = 0;
+        for(auto num : nums){
+            if(map[k - num]>0){
+                map[k-num] --;
+                count++;
+            }
+            else{
+                map[num] ++;
+            }
+        }
+        return count;
+    }
+};

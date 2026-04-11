ca4755c483b4a1961887d082aca7627afd9b0303
Nam <89253211+kymahns@users.noreply.github.com>
2026-04-11 15:10:49 +0100

    Time: 0 ms (100.00%) | Memory: 18.9 MB (96.56%) - LeetSmith

---
+++ DSA/104-maximum-depth-of-binary-tree/solution.cpp
@@ -0,0 +1,28 @@
+// Problem: Maximum Depth of Binary Tree
+// URL: https://leetcode.com/problems/maximum-depth-of-binary-tree
+// Difficulty: Easy
+// Language: C++
+// Date: 2026-04-11
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left,right) + 1;
+    }
+};

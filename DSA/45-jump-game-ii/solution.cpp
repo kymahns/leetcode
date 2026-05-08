c7b344c6eac165f49bf5d8484bcf802352c0d368
Nam <89253211+kymahns@users.noreply.github.com>
2026-05-08 23:54:31 +0100

    Time: 0 ms (100.00%) | Memory: 20.5 MB (59.60%) - LeetSmith

---
+++ DSA/45-jump-game-ii/solution.cpp
@@ -0,0 +1,22 @@
+// Problem: Jump Game II
+// URL: https://leetcode.com/problems/jump-game-ii
+// Difficulty: Medium
+// Language: C++
+// Date: 2026-05-08
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int near = 0, far = 0, jumps = 0;
+        while (far < nums.size() - 1) {
+            int farthest = 0;
+            for (int i = near; i <= far; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+        return jumps;        
+    }
+};

a6ac94808215c33ea1a4c3147834ef895e188f3e
Nam <89253211+kymahns@users.noreply.github.com>
2026-05-16 13:00:08 +0100

    Time: 0 ms (100.00%) | Memory: 16.2 MB (63.69%) - LeetSmith

---
+++ DSA/154-find-minimum-in-rotated-sorted-array-ii/solution.cpp
@@ -0,0 +1,25 @@
+// Problem: Find Minimum in Rotated Sorted Array II
+// URL: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii
+// Difficulty: Hard
+// Language: C++
+// Date: 2026-05-16
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size()-1;
+        while(left <right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] < nums[right]){
+                right = mid;
+            }
+            else if(nums[mid] > nums[right]){
+                left = mid + 1;
+            }
+            else{
+                right --;
+            }
+        }
+        return nums[left];
+    }
+};

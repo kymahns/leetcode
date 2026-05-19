ba672f16b1dd636eb5eee13106a811e11ec5548d
Nam <89253211+kymahns@users.noreply.github.com>
2026-05-19 01:32:15 +0100

    Time: 71 ms (88.96%) | Memory: 79.5 MB (57.52%) - LeetSmith

---
+++ DSA/1345-jump-game-iv/solution.cpp
@@ -0,0 +1,43 @@
+// Problem: Jump Game IV
+// URL: https://leetcode.com/problems/jump-game-iv
+// Difficulty: Hard
+// Language: C++
+// Date: 2026-05-19
+
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n=arr.size();
+        if(n==1)return 0;
+        unordered_map<int,vector<int>>mp;
+        for (int i=0;i<n;i++){
+            mp[arr[i]].push_back(i);
+        }
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vector<int>vis(n,0);
+        vis[0]=1;
+        while(!q.empty()){
+            int node=q.front().first;
+            int dist=q.front().second;
+            q.pop();
+            if(node==n-1)return dist;
+            if(node-1>=0 && !vis[node-1]){
+                vis[node-1]=1;
+                q.push({node-1,dist+1});
+            }
+            if(node+1<n && !vis[node+1]){
+                vis[node+1]=1;
+                q.push({node+1,dist+1});
+            }
+            for (int next:mp[arr[node]]){
+                if (!vis[next]){
+                    vis[next]=1;
+                    q.push({next,dist+1});
+                }
+            }
+            mp[arr[node]].clear();
+        }
+        return -1;
+    }
+};

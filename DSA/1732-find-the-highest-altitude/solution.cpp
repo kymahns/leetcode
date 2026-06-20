591fb6d97397db64fd8dc2b91c0231081285709c
Nam <89253211+kymahns@users.noreply.github.com>
2026-06-20 17:21:10 +0100

    Time: 0 ms (100.00%) | Memory: 11.5 MB (11.05%) - LeetSmith

--- DSA/1732-find-the-highest-altitude/solution.cpp
+++ DSA/1732-find-the-highest-altitude/solution.cpp
@@ -2,17 +2,19 @@
 // URL: https://leetcode.com/problems/find-the-highest-altitude
 // Difficulty: Easy
 // Language: C++
-// Date: 2026-06-20
+// Date: 2026-04-04
 
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int curHeight = 0;
-        int maxHeight = 0;
-        for(int i = 0; i < gain.size(); i++){
-            curHeight = curHeight + gain[i];
-            maxHeight = max(maxHeight,curHeight);
+        vector<int> alt(gain.size()+1,0);
+        for(int i = 0; i<gain.size();i++){
+            alt[i+1] = alt[i] +gain[i];
         }
-        return maxHeight;
+        int maxim = 0;
+        for(auto i : alt){
+            maxim = max(maxim,i);
+        }
+        return maxim;
     }
 };

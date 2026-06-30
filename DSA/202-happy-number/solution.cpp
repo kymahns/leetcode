786aa6c98cce8c32e17d4783892f864cd00ee9b9
Nam <89253211+kymahns@users.noreply.github.com>
2026-06-30 17:37:15 +0100

    Time: 0 ms (100.00%) | Memory: 8.5 MB (22.51%) - LeetSmith

---
+++ DSA/202-happy-number/solution.cpp
@@ -0,0 +1,35 @@
+// Problem: Happy Number
+// URL: https://leetcode.com/problems/happy-number
+// Difficulty: Easy
+// Language: C++
+// Date: 2026-06-30
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> visit;
+        
+        while (visit.find(n) == visit.end()) {
+            visit.insert(n);
+            n = getNextNumber(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+private:
+    int getNextNumber(int n) {
+        int output = 0;
+        
+        while (n > 0) {
+            int digit = n % 10;
+            output += digit * digit;
+            n = n / 10;
+        }
+        
+        return output;
+    }
+};

098237ed13422a0e4b287d5ba300494d13366d19
Nam <89253211+kymahns@users.noreply.github.com>
2026-07-15 15:40:27 +0700

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSmith

---
+++ DSA/3658-gcd-of-odd-and-even-sums/solution.cpp
@@ -0,0 +1,12 @@
+// Problem: GCD of Odd and Even Sums
+// URL: https://leetcode.com/problems/gcd-of-odd-and-even-sums
+// Difficulty: Easy
+// Language: C++
+// Date: 2026-07-15
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
